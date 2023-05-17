a94d892a056a4562942454cca4f17278c43b6453
Sannamari <sannamari.pynnonen@tuni.fi>
2023-05-17 13:12:40 +0300

    Exercise 2.1 kurssitiedot step6

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,66 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />  
+      )}
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
