b346f4885f00ab7e92e32bb6bdc1565f7abcf6ac
Sannamari <sannamari.pynnonen@tuni.fi>
2023-05-17 11:52:12 +0300

    Exercise 1.14 anekdootit step3

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(new Uint8Array(8))
+  const [mostVoted, setMostVoted] = useState(0)
   
   const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
@@ -22,19 +23,30 @@ const App = () => {
   const handleClick = () => {
     setSelected(randomNumber(0, 7))
   }
+
+  const checkMostVoted = (points) => {
+    const max = Math.max(...points)
+    const index = points.indexOf(max)
+    setMostVoted(index)
+  }
   
   const voteAnecdote = () => {
     const copy = [...points]
     copy[selected] += 1
     setPoints(copy)
+    checkMostVoted(copy)
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
       <button onClick={voteAnecdote}>vote</button>
       <button onClick={handleClick}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVoted]}</p>
+      <p>has {points[mostVoted]} votes</p>
     </div>
   )
 }
