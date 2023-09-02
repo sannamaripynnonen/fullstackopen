542802ed5eca64ba145cb0364618dec95b2a4411
Sannamari <sannamari.pynnonen@tuni.fi>
2023-09-02 17:10:55 +0300

    Exercise 4.2 blogilista step2

---
+++ osa4/app.js
@@ -0,0 +1,24 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blog')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
