1d03c75cd08e143cfc885f313384e0d186a5cf55
Sannamari <sannamari.pynnonen@tuni.fi>
2023-09-02 19:39:44 +0300

    Exercise 4.4 apufunktioita ja yksikkötestejä, step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,19 @@
-const dummy = (blogs) => {
-    return 1
+const blog = require("../models/blog");
+
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    } else if (blogs.length === 1) {
+        return blogs[0].likes
+    } else {
+        let result = 0
+        blogs.forEach(blog => {
+            result += blog.likes
+        });
+        return result
+    }
 }
 
 module.exports = {
-    dummy
+    totalLikes
 }
