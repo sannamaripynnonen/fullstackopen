5b32182cc9b87ed2c67736195650a280d5d64b1c
Sannamari <sannamari.pynnonen@tuni.fi>
2024-04-08 13:33:06 +0300

    Exercise 4.9 blogilistan testit, step2

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,4 +1,5 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -10,6 +11,16 @@ test.only('blogs are returned as json', async () => {
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test.only('blogs are identified by id', async () => {
+    const response = await api.get('/api/blogs')
+
+    if (response.body.length > 0) {
+        assert.ok(response.body[0].id != undefined)
+    } else {
+        assert.fail('No blogs returned')
+    }
 })
 
 after(async () => {
