1db722f81bc48f48b953831ea8ca4d4f9aadccbd
Sannamari <sannamari.pynnonen@tuni.fi>
2024-04-08 14:12:55 +0300

    Exercise 4.11 blogilistan testit, step4

--- osa4/controllers/blog.js
+++ osa4/controllers/blog.js
@@ -10,7 +10,13 @@ blogRouter.get('/', (request, response) => {
   })
   
 blogRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+    })
   
     blog
       .save()
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -6,14 +6,14 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test.only('blogs are returned as json', async () => {
+test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
 
-test.only('blogs are identified by id', async () => {
+test('blogs are identified by id', async () => {
     const response = await api.get('/api/blogs')
 
     if (response.body.length > 0) {
@@ -23,7 +23,7 @@ test.only('blogs are identified by id', async () => {
     }
 })
 
-test.only('new blog is added', async () => {
+test('new blog is added', async () => {
     const newBlog = {
         title: 'New blog title',
         author: 'New blog author',
@@ -44,6 +44,21 @@ test.only('new blog is added', async () => {
     assert.strictEqual(response.body.length, initialBlogs.length +1)
 })
 
+test.only('likes is 0 when value is not given', async () => {
+    const newBlog = {
+        title: 'New title',
+        author: 'New author',
+        url: 'https://example.com/'
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    assert.strictEqual(response.body.likes, 0)
+})
 
 after(async () => {
     await mongoose.connection.close()
